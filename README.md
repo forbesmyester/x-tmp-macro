f8e9902114b39a7e19da29896c15db57dfd4d299
Matthew Forrester <matt@keyboardwritescode.com>
2014-03-04 17:31:48 +0000

    Integrate it all together so it's a bit less hacky

--- README.md
+++ README.md
@@ -1,15 +1,13 @@
 # x-tmp-macro
 
-A collection of script that allows storing and execution of temporary macros within the Linux X Windows environment
+A hacky node.js script that allows storing and execution of temporary macros within the Linux X Windows environment
 
 ## Requirements
 
 You must have `xmacro` and `xdotool` installed within your $PATH.
 
-## Scripts
-
-**x-tmp-macro-record:** Starts a xmacro recording process saving the contents to `/tmp/x-tmp-macro`.
-
-**index.js:** Takes STDIN input from a `xmacrorec2` file and converts it into a series of `xdotool` statements which is outputted to STDOUT.
-
-**x-tmp-macro-play [filename]:** Takes the contents of an xmacro file from `filename` (defaults to `/tmp/x-tmp-macro`) which is then transformed using `index.js` and then executed using `xdotool`.
+## Usage:
+    index.js create: Starts a xmacro recording process saving the temporary play.
+    index.js transform: Takes STDIN input from a `xmacrorec2` file and converts it into a series of `xdotool` statements which is outputted to STDOUT.
+    index.js run [name]: Reads a stored play, transforms the contents using `index.js transform` and runs it.
+    index.js save [name]: Saves the temporary play into the play name specified.
--- index.js
+++ index.js
@@ -4,6 +4,11 @@
 
 "use strict";
 
+var STORAGE_LOCATION = '/tmp',
+	DEFAULT_RECORD_NAME = 'temporary',
+	LOG_LOCATION = STORAGE_LOCATION + '/x-tmp-macro.log',
+	TMP_RUN_FILE = '/tmp/x-tmp-macro.run';
+
 var modifiers = [
 	'Shift_L',
 	'Control_L',
@@ -66,21 +71,159 @@ var map_to_xdotool_script = function(codified) {
 	return 'xdotool key --clearmodifiers --delay 1 ' + ks.join('+') + "\n";
 };
 
-var current_line = '';
-process.stdin.resume();
-process.stdin.on('data', function(data) {
-	var lines = data.toString().split("\n"),
-		i, l;
-	
-	for (i=0, l=lines.length; i<l ; i++) {
-		current_line = current_line + lines[i];
-		process.stdout.write(
-			map_to_xdotool_script(codify_line(current_line))
-		);
-		if (i !== lines.length-1) {
-			current_line = '';
+var transform = function() {
+	var current_line = '';
+	process.stdin.resume();
+	process.stdin.on('data', function(data) {
+		var lines = data.toString().split("\n"),
+			i, l;
+		
+		for (i=0, l=lines.length; i<l ; i++) {
+			current_line = current_line + lines[i];
+			process.stdout.write(
+				map_to_xdotool_script(codify_line(current_line))
+			);
+			if (i !== lines.length-1) {
+				current_line = '';
+			}
 		}
+	});
+};
+
+var print_help = function() {
+	/* global console: false, __filename */
+	var help = [
+		"Usage:",
+		"    " + __filename.replace(/.*\//,'') + " create: Starts a xmacro recording process saving the temporary play.",
+		"    " + __filename.replace(/.*\//,'') + " transform: Takes STDIN input from a `xmacrorec2` file and converts it into a series of `xdotool` statements which is outputted to STDOUT.",
+		"    " + __filename.replace(/.*\//,'') + " run [name]: Reads a stored play, transforms the contents using `index.js transform` and runs it.",
+		"    " + __filename.replace(/.*\//,'') + " save [name]: Saves the temporary play into the play name specified."
+		].join("\n");
+	console.log(help);
+};
+
+var construct_path_from_play_name = function(play_name) {
+	return STORAGE_LOCATION + "/" + play_name + '.x-tmp-macro';
+};
+
+var run = function(play_name) {
+	
+	var commands = [
+			// 'killall xmacrorec2',
+			'cat PLAY_LOCATION | node THIS_SCRIPT transform > TMP_RUN_FILE',
+			'chmod +x TMP_RUN_FILE',
+			'TMP_RUN_FILE',
+			'rm TMP_RUN_FILE'
+		],
+		fs = require('fs');
+	
+	if (!fs.existsSync(construct_path_from_play_name(play_name))) {
+		fs.writeFileSync(
+				LOG_LOCATION,
+				"your play_name (" + play_name + ") does not exist."
+			);
+		return;
 	}
-});
+	
+	var q = require('async').queue(function(command, next) {
+		/* global __filename: false */
+		command = command.replace(
+			/PLAY_LOCATION/, construct_path_from_play_name(play_name)
+		);
+		command = command.replace(/THIS_SCRIPT/, __filename);
+		command = command.replace(/TMP_RUN_FILE/, TMP_RUN_FILE);
+		require('child_process').exec(command, function(err) {
+			if (err) {
+				fs.writeFileSync(
+					LOG_LOCATION,
+					"Command '" + command + "' exited with error " + err
+				);
+			}
+			return next(err);
+		});
+	});
+	
+	for (var i=0; i<commands.length; i++) {
+		q.push(commands[i]);
+	}
+};
+
+var create = function() {
+	console.log("CREATE");
+	var cmd = 'xmacrorec2 -k 9 > ' + construct_path_from_play_name(DEFAULT_RECORD_NAME);
+	
+	require('child_process').exec(cmd, function(err) {
+		if (err) {
+			require('fs').writeFileSync(
+				LOG_LOCATION,
+				"Command '" + cmd + "' exited with error " + err
+			);
+		}
+	});
+};
+
+var save = function(play_name) {
+	
+	var fs = require('fs'),
+		tmp = STORAGE_LOCATION + "/" + DEFAULT_RECORD_NAME + '.x-tmp-macro';
+	
+	if (!fs.existsSync(tmp)) {
+		fs.writeFileSync(
+				LOG_LOCATION,
+				"there is no tempoary play to save."
+			);
+		return;
+	}
+	
+	fs.writeFileSync(
+		construct_path_from_play_name(play_name),
+		fs.readFileSync(tmp)
+	);
+};
+
+var get_play_name = function(process_arguments) {
+
+	var play_name = DEFAULT_RECORD_NAME,
+		fs = require('fs');
+	
+	if (process_arguments.length > 3) {
+		play_name = process_arguments[3];
+	}
+	
+	if (play_name.match(/\//)) {
+		fs.writeFileSync(
+				LOG_LOCATION,
+				"your play_name (" + play_name + ") should not include a '/'."
+			);
+		return;
+	}
+	
+	return play_name;
+	
+};
+	
+var operations = {
+	transform: transform.bind(this, get_play_name(process.argv)),
+	create: create,
+	run: run.bind(this, get_play_name(process.argv)),
+	save: save.bind(this, get_play_name(process.argv))
+};
+
+
+var valid_operations = (function(operations) {
+		var r = [];
+		for (var k in operations) { if (operations.hasOwnProperty(k)) {
+			r.push(k);
+		} }
+		return r;
+	}(operations));
+
+if (
+	valid_operations.indexOf(process.argv[2]) === -1) {
+	print_help();
+	return;
+}
+
+operations[process.argv[2]].apply(process.argv.slice(3));
 
 }());
